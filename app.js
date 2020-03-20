2382faeb3de6491769a8c183a2af66ff34194319
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-03-20 14:53:03 -0400

    added functions to highlight a key on the board when the corresponding key is pressed, and removes highlight upon keyup. functions dont work completely the way i want, as if any key is released, all highlights are removed, even if other keys are still being pressed.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 $('#keyboard-upper-container').hide();
+
+//function toggles to show upper keyboard if shift key is pressed down
 $(document).keydown(function (e) {
     if (e.shiftKey == true) {
         $('#keyboard-upper-container').show();
         $('#keyboard-lower-container').hide();
-    } 
+    }
 })
-$(document).keyup(function () {
-    $('#keyboard-upper-container').hide();
-    $('#keyboard-lower-container').show();
+
+//function toggles to show lower keyboard if shift key is released
+$(document).keyup(function (e) {
+    if (e.shiftKey == false) {
+        $('#keyboard-upper-container').hide();
+        $('#keyboard-lower-container').show();
+    }
+})
+//function gets the id of the key that is pressed, and gives it a class that has a background color of yellow.
+$(document).keypress(function (e) {
+    $('#' + e.keyCode).addClass('keyHighlight');
+})
+//function takes away the class when the key is no longer pressed.
+$(document).keyup(function (e) {
+    $('.keyHighlight').removeClass('keyHighlight');
 })
